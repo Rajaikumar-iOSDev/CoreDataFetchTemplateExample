8565b10d848341406731c119dedb2157897a850d
Rajai Kumar <writetorajai@outlook.com>
2021-02-21 00:01:35 +0530

    Initial Commit

---
+++ CoreDataFetchTemplateExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataFetchTemplateExample
+//
+//  Created by Admin on 21/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
